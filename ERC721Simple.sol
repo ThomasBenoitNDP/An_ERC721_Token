cc4cdae2b6679a07cfcefe1f5b5acc5cc20c7468
Thomas BENOIT <52998748+ThomasBenoitNDP@users.noreply.github.com>
2019-12-05 11:41:51 +0100

    Create ERC721Simple.sol

---
+++ ERC721Simple.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.1;
+
+contract ERC721Simple {
+ event Transfer(address indexed _from, address indexed _to, uint256 _tokenId);
+
+ function balanceOf(address _owner) public view returns (uint256 balance);
+ function ownerOf(uint256 _tokenId) public view returns (address _owner);
+ function exists(uint256 _tokenId) public view returns (bool exists);
+
+ function transferFrom(address _from, address _to, uint256 _tokenId) public;
+}
