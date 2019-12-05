327b196a708381f316573b7db2385003155c1dc5
Thomas BENOIT <52998748+ThomasBenoitNDP@users.noreply.github.com>
2019-12-05 12:50:47 +0100

    Create SomeRegister.sol

---
+++ SomeRegister.sol
@@ -0,0 +1,22 @@
+contract SomeRegister {
+   address backendContract;
+   address[] previousBackends;
+   address owner;
+
+   function SomeRegister() {
+       owner = msg.sender;
+
+   }   modifier onlyOwner() {
+       require(msg.sender == owner)
+       _;   }
+
+   function changeBackend(address newBackend) public   onlyOwner()   returns (bool)   {
+       if(newBackend != backendContract) {
+           previousBackends.push(backendContract);
+           backendContract = newBackend;
+           return true;
+       }       
+	return false;   
+	} 
+
+}
